6bad433c75253e4cedf2bde3897efd46945be936
Serjio <sergei.tiutriumov@gorfax.ru>
2024-03-14 21:41:13 +0300

    f

--- Python/pr_1/posts/models.py
+++ Python/pr_1/posts/models.py
@@ -5,4 +5,17 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+# GENRE_CHOICES = (
+#     ("R", "Rock"),
+#     ("E", "Pop"),
+#     ("C", "Classic"),
+#     ("O", "Saundtracks"),
+# )
+# class CD(models.Model):
+#     title = models.CharField(max_length=100)
+#     description = models.TextField(null=True, blank=True)
+#     artist = models.CharField(max_length=40)
+#     date = models.DateField()
+#     genre = models.CharField(max_length=1, choices=GENRE_CHOICES)
+
     
