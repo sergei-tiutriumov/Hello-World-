964b631aaee1f66783ccd49ee5ae4cb6d77c6106
Serjio <sergei.tiutriumov@gorfax.ru>
2024-03-16 16:32:03 +0300

    kkk

--- Python/pr_1/posts/views.py
+++ Python/pr_1/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+def index(request):
+    latest = Post.objects.order_by('-pub_date')[:10]
+    # output = []
+    # for item in latest:
+    #     output.append(item.text)
+    return render(request, "index.html",{"posts": latest})
 
 # Create your views here.
--- Python/pr_1/pr_1/settings.py
+++ Python/pr_1/pr_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,11 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'pr_1.urls'
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
